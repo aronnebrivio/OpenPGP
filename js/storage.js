c104c55da5e80926b96cc11d019f91c34d66cca1
Aronne Brivio <aroblu94@gmail.com>
2015-05-14 17:35:10 +0200

    Added License, #10

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,24 @@
-/* LOCAL STORAGE */
+/*
+ * OpenPGP - PGP enryption  for Firefox OS
+ * Copyright (C) 2015 Aronne Brivio
+ *
+ * This file is part of OpenPGP.
+ *
+ * OpenPGP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenPGP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenPGP.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-/* Using js Object Oriented */
+/* LOCAL STORAGE */
 function DB() {
 	var db = init();
 	
